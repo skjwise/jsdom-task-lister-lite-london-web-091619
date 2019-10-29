a03e872f04496b9fb07e421ddbd5e6a346cafe8b
skjwise <skjwise@hotmail.com>
2019-10-29 16:23:33 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const submit = document.querySelector('#create-task-form')
+  const task = document.querySelector('#tasks')
+  
+  submit.addEventListener('submit', function(e){
+    e.preventDefault()
+    
+    const li = document.createElement('li') //create element
+    const taskInput = document.querySelector('#new-task-description')
+    li.innerText = taskInput.value //populated it
+    task.append(li) //so we are updating li and task
+    
+    const button = document.createElement('button') //create button
+    
+     button.innerText = 'x' 
+     task.append(button) //updating 
+     
+      button.addEventListener('click', function(){
+      task.removeChild(li)
+      task.removeChild(button)
+    })
+  
+  })
 });
