776958b75a92ece33651120fcd5a1ebf242fced7
skjwise <skjwise@hotmail.com>
2019-11-19 14:08:03 +0000

    added prioritydropdown feature

--- src/index.js
+++ src/index.js
@@ -1,24 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   const submit = document.querySelector('#create-task-form')
   const task = document.querySelector('#tasks')
+  // const form = document.querySelector('form')
   
   submit.addEventListener('submit', function(e){
     e.preventDefault()
-    
-    const li = document.createElement('li') //create element
+
+    const li = document.createElement('li') //create a li element
     const taskInput = document.querySelector('#new-task-description')
     li.innerText = taskInput.value //populated it
-    task.append(li) //so we are updating li and task
+
+    const p = document.createElement('p')
+    const dateInput = document.querySelector('#due-date')
+    p.innerText = dateInput.value
+
+    //select the dropdown options and append the value to the To Dos
+    const h4 = document.createElement('h4')
+    const prioityInput = document.querySelector('select')
+    h4.innerText = prioityInput.value
+    task.append(li, p, h4) //so we are updating li, p and task
     
     const button = document.createElement('button') //create button
-    
-     button.innerText = 'x' 
+     button.id = button.id
+     button.innerText = 'Delete' 
      task.append(button) //updating 
      
       button.addEventListener('click', function(){
-      task.removeChild(li)
-      task.removeChild(button)
+      task.remove(li, p, h4) //created a click event that removes the elements in the To Dos
+      task.removeChild(button) //at the same time we delete the button
     })
+
+    document.getElementById('')
+    function changeColor(){
+      document.querySelector('#red')
+      document.
+
+    }
   
   })
 });
